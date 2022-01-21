00ed699e68ec7d3eb9f4d3c855c2d11be1cd7aa2
Hindi_Estebel <fluffycat13@yandex.ru>
2022-01-21 17:26:10 +0300

    Added button start/stop, but code doesn't works because of the Thread problems

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements Runnable{
     private MediaPlayer mediaPlayer;
     private static final String TAG = "MyActivity_output";
     long time;
+    boolean is_running;
     Thread timerThread;
 
     private AudioManager.OnAudioFocusChangeListener audioFocusChangeListener =
@@ -51,43 +52,44 @@ public class MainActivity extends AppCompatActivity implements Runnable{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        is_running = false;
         setContentView(binding.getRoot());
         AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         mediaPlayer = MediaPlayer.create(this, R.raw.stopper);
-        binding.buttonRun.setOnClickListener(v -> {
-            Log.i(TAG, "Button clicked");
-            if (timer != null) {
-                timer.cancel();
-            }
-            timer = new Timer();
-            long time_hours;
-            long time_sec;
-            long time_min;
-            String text_min = binding.editTextTimeMin.getText().toString();
-            String text_hours = binding.editTextTimeHours.getText().toString();
-            String text_sec = binding.editTextTimeSeconds.getText().toString();
-            if (!text_min.equals("")) {
-                time_min = Long.parseLong(text_min) * 1000 * 60;
-            }
-            else {
-                time_min = 0;
-            }
-            if (!text_sec.equals("")) {
-                time_sec = Long.parseLong(text_sec) * 1000;
-            }
-            else {
-                time_sec = 0;
-            }
-            if (!text_hours.equals("")) {
-                time_hours = Long.parseLong(text_hours) * 1000 * 3600;
-            }
-            else {
-                time_hours = 0;
-            }
+        binding.buttonRunStop.setOnClickListener(v -> {
+            if (!is_running) {
+                Log.i(TAG, "Button clicked");
+                is_running = true;
+                if (timer != null) {
+                    timer.cancel();
+                }
+                binding.buttonRunStop.setText(R.string.cancel);
+                timer = new Timer();
+                long time_hours;
+                long time_sec;
+                long time_min;
+                String text_min = binding.editTextTimeMin.getText().toString();
+                String text_hours = binding.editTextTimeHours.getText().toString();
+                String text_sec = binding.editTextTimeSeconds.getText().toString();
+                if (!text_min.equals("")) {
+                    time_min = Long.parseLong(text_min) * 1000 * 60;
+                } else {
+                    time_min = 0;
+                }
+                if (!text_sec.equals("")) {
+                    time_sec = Long.parseLong(text_sec) * 1000;
+                } else {
+                    time_sec = 0;
+                }
+                if (!text_hours.equals("")) {
+                    time_hours = Long.parseLong(text_hours) * 1000 * 3600;
+                } else {
+                    time_hours = 0;
+                }
                 time = time_hours + time_sec + time_min;
-            if(time == 0) {
-                return;
-            }
+                if (time == 0) {
+                    return;
+                }
                 Log.i(TAG, "Time: " + time + " wil be start on schedule");
                 timerThread = new Thread(MainActivity.this);
                 timerThread.start();
@@ -102,42 +104,43 @@ public class MainActivity extends AppCompatActivity implements Runnable{
                         mediaPlayer.start();
                         Log.i(TAG, "Payer started!");
                         audioManager.abandonAudioFocus(audioFocusChangeListener);
+                        binding.buttonRunStop.setText(R.string.run);
                     }
                 }, time);
-
-
-        });
-        binding.buttonCancel.setOnClickListener(v -> {
-            Log.i(TAG, "Cancel clicked");
-            if (timer != null) {
-                timer.cancel();
-                timer = null;
-                timerThread.interrupt();
-                try {
-                    timerThread.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            }
+            else {
+                is_running = false;
+                binding.buttonRunStop.setText(R.string.run);
+                if (timer != null) {
+                    timer.cancel();
+                    timer = null;
+                    timerThread.interrupt();
+                    try {
+                        timerThread.join();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
     }
 
     @Override
-    public void run() {
-        do {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    binding.timeshower.setText("" + (time / 1000));
-                }
-            });
-            time -= 1000;
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        while (time > 0);
-    }
+    public void run() {}
+//        do {
+//            runOnUiThread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    binding.timeshower.setText("" + (time / 1000));
+//                }
+//            });
+//            time -= 1000;
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        while (time > 0);
+//    }
 }
